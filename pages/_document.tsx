be129bfd1ab55b35d3f6de6acd47f9cd2293d937
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-09 16:52:36 -0300

    template script setup

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" /> */}
+
+        <link href="/favicon.ico" rel="icon" type="image/ico" sizes="16x16" />
+        {/* <link rel="apple-touch-icon" href="/apple-icon.png"></link> */}
+
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <meta name="theme-color" content="#317EFB" />
+
+        <Script
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+          src="https://polyfill.io/v3/polyfill.min.js"
+        />
+      </Head>
+
+      <body>
+        <Main />
+
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
