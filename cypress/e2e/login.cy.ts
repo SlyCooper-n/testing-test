c4e856b5256b31356de5078a999e46186024a002
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-15 18:45:14 -0300

    chore: first cypress e2e test

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("Login", () => {
+  it("should login", () => {
+    cy.visit("/");
+    cy.get("#email").type("valid@email.com");
+    cy.get("#password").type("password");
+    cy.get("#confirm-pw").type("password");
+    cy.get(".btn").click();
+
+    cy.url().should("include", "/home");
+  });
+});
