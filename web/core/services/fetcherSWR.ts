e8f1f725d42452abcb5459b2573ba0fca8fb07ff
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-16 20:59:41 -0300

    chore: SWR fetcher function

---
+++ web/core/services/fetcherSWR.ts
@@ -0,0 +1,6 @@
+import { RewardsType } from "@core/types";
+import axios from "axios";
+import { Fetcher } from "swr";
+
+export const fetcher: Fetcher<RewardsType[], string> = (url) =>
+  axios.get("http://localhost:3333" + url).then((res) => res.data);
