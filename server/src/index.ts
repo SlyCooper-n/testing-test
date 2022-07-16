c0bc5642167d7bebc8b2cbd39cb295e554e1c386
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-16 20:57:41 -0300

    chore: server is getting post request now

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,28 @@
+import cors from "cors";
 import express from "express";
+import { rewardsData } from "./utils/rewardsData";
 
 const app = express();
 
+app.use(cors({}));
 app.use(express.json());
 
-app.route("/rewards").get((req, res) => {
-  res.send("Hello World!").status(200);
-});
+app
+  .route("/rewards")
+  .get((req, res) => {
+    return res.status(200).json(rewardsData);
+  })
+  .post((req, res) => {
+    rewardsData.push({
+      id: rewardsData.length + 1,
+      title: req.body.title,
+      body: req.body.body,
+      valid: req.body.valid,
+      points: Math.ceil(Math.random() * 1000),
+    });
+
+    return res.status(201).send();
+  });
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
---
+++ server/src/utils/rewardsData.ts
@@ -0,0 +1,51 @@
+export const rewardsData = [
+  {
+    id: 1,
+    title: "Best goal ever",
+    body: "I am the best player at school",
+    valid: true,
+    points: 532,
+  },
+  {
+    id: 2,
+    title: "Giant lego castle",
+    body: "I've built a super giant castle with lego bricks",
+    valid: true,
+    points: 792,
+  },
+  {
+    id: 3,
+    title: "Best kid ever",
+    body: "My parents love me",
+    valid: true,
+    points: 649,
+  },
+  {
+    id: 4,
+    title: "Long song with guitar",
+    body: "Today I've played the longest song I've ever heard with my guitar",
+    valid: true,
+    points: 423,
+  },
+  {
+    id: 5,
+    title: "In memory of Eddie Munson",
+    body: "I played Master of Puppets in memory of Eddie Munson 😭",
+    valid: true,
+    points: 1000000000,
+  },
+  {
+    id: 6,
+    title: "Best dev",
+    body: "I learning everything!!!",
+    valid: true,
+    points: 925,
+  },
+  {
+    id: 7,
+    title: "Better person",
+    body: "I'm striving to be better every single day",
+    valid: true,
+    points: 843,
+  },
+];
