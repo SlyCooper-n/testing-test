78dd6ae7aa04707c3665596ecefa884e420335bb
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-16 17:21:40 -0300

    .

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+
+export {};
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -12,19 +15,26 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
+
+// Cypress.Commands.addAll({
+//   login() {
+//     cy.visit("/");
+//     cy.get("#email").type("valid@email.com");
+//     cy.get("#password").type("password");
+//     cy.get("#confirm-pw").type("password");
+//     cy.get(".btn").click();
+//   },
+// });
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
+
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
+
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
+
 // declare global {
 //   namespace Cypress {
 //     interface Chainable {
