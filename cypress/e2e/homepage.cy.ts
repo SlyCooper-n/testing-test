6d53ce78ce8f5f758682003a82b76da5579c1c9d
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-16 18:10:22 -0300

    chore: tests for habit and accomplishments app views

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -2,7 +2,14 @@
 
 import { login } from "./login.cy";
 
+export {};
+
 describe("Homepage", () => {
+  // beforeEach(() => {
+  //   cy.visit("/");
+  //   login();
+  // });
+
   it("should render app view correctly", () => {
     cy.visit("/");
     login();
@@ -14,15 +21,48 @@ describe("Homepage", () => {
     cy.contains("h2", "Accomplishments").should("exist");
   });
 
-  it("should be able to add new habit", () => {
-    cy.visit("/");
-    login();
+  describe("Habit Checklist", () => {
+    it("should be able to add new habit", () => {
+      cy.get(".hidden > :nth-child(1) > .btn").click();
+      cy.get(".input").type("New Habit");
 
-    cy.get(".hidden > :nth-child(1) > .btn").click();
-    cy.get(".input").type("New Habit");
+      cy.get(".mb-8 > .btn").click();
 
-    cy.get(".mb-8 > .btn").click();
+      cy.contains("New Habit").should("exist");
+    });
 
-    cy.contains("New Habit").should("exist");
+    it("should be able to toggle habit", () => {
+      cy.contains("New Habit").click();
+      cy.get(".text-success").should("exist");
+      cy.get(".text-error").should("not.exist");
+
+      cy.contains("New Habit").click();
+      cy.get(".text-success").should("not.exist");
+      cy.get(".text-error").should("exist");
+    });
+  });
+
+  describe("Accomplishments", () => {
+    it("should be able to add new accomplishment", () => {
+      cy.get(".hidden > :nth-child(2) > .btn").click();
+
+      cy.get("input.input").type("New Accomplishment");
+      cy.get(".h-32").type("New Accomplishment Description");
+      cy.get(".label-text").click();
+
+      cy.get(".form-control > .btn").click();
+
+      cy.contains(/accomplishment sent successfully/i).should("exist");
+    });
+
+    it("should return to Accomplishments dashboard when 'Go back' button is clicked", () => {
+      cy.contains("button", "Go back").click();
+
+      cy.contains("h2", "Accomplishments").should("be.visible");
+
+      cy.get("input.input").should("have.value", "");
+      cy.get(".h-32").should("have.value", "");
+      cy.get(".label-text").should("not.be.checked");
+    });
   });
 });
