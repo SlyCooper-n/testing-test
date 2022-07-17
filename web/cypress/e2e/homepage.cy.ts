1cf489524a7c945db61597944c51c7a335537a5f
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-16 22:01:11 -0300

    chore: working with mocked HTTP requests

--- web/cypress/e2e/homepage.cy.ts
+++ web/cypress/e2e/homepage.cy.ts
@@ -39,6 +39,18 @@ describe("Homepage", () => {
     it("should be able to add new accomplishment", () => {
       cy.get(".hidden > :nth-child(2) > .btn").click();
 
+      cy.intercept("POST", "http://localhost:3333/rewards", (req) => {
+        const body = req.body;
+        expect(body.title).to.equal("New Accomplishment");
+        expect(body.body).to.equal("New Accomplishment Description");
+        expect(body.valid).to.equal(false);
+
+        return {
+          status: 201,
+          body: {},
+        };
+      }).as("postReward");
+
       cy.get("input.input").type("New Accomplishment");
       cy.get(".h-32").type("New Accomplishment Description");
       cy.get(".label-text").click();
@@ -56,6 +68,65 @@ describe("Homepage", () => {
       cy.get("input.input").should("have.value", "");
       cy.get(".h-32").should("have.value", "");
       cy.get(".checkbox").should("be.checked");
+    });
+  });
+
+  describe("Rewards", () => {
+    it("should render mocked rewards", () => {
+      cy.intercept("GET", "http://localhost:3333/rewards", {
+        fixture: "rewards.json",
+      });
+
+      cy.get(".hidden > :nth-child(3) > .btn").click();
+
+      cy.contains(/eddie munson/i).should("be.visible");
+      cy.contains(/best dev/i).should("be.visible");
+      cy.contains(/better person/i).should("be.visible");
+    });
+
+    it("should show error when content is not appropriate", () => {
+      cy.intercept("POST", "http://localhost:3333/rewards", (req) => {
+        if (req.body.title.includes("bad") || req.body.body.includes("bad")) {
+          req.reply((res) =>
+            res.send({
+              status: 406,
+              body: {
+                msg: "Your content is not appropriate",
+              },
+            })
+          );
+        }
+      });
+
+      cy.get(".hidden > :nth-child(2) > .btn").click();
+
+      cy.get("input.input").type("Bad Accomplishment");
+      cy.get(".h-32").type("New bad Accomplishment Description");
+      cy.get(".label-text").click();
+
+      cy.get(".form-control > .btn").click();
+
+      cy.contains(/accomplishment sent successfully/i).should("not.exist");
+      cy.contains(/your content is not appropriate/i).should("be.visible");
+    });
+
+    describe("handling with real server", () => {
+      it("should render new accomplishments", () => {
+        cy.visit("/");
+        login();
+
+        cy.get(".hidden > :nth-child(2) > .btn").click();
+
+        cy.get("input.input").type("New Accomplishment");
+        cy.get(".h-32").type("New Accomplishment Description");
+        cy.get(".label-text").click();
+
+        cy.get(".form-control > .btn").click();
+
+        cy.get(".hidden > :nth-child(3) > .btn").click();
+
+        cy.contains(/new accomplishment/i).should("be.visible");
+      });
     });
   });
 });
